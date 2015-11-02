f40d96c2d411c0412ba4f258e002a6fd88cfaaea
YuraFedorchuk <imafost758@gmail.com>
2015-11-02 20:42:00 +0200

    php extension

---
+++ index.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Продающее видео</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Andika&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/style.css" media="screen" charset="utf-8">
+  </head>
+  <body>
+      <div class="header" style="background: url(img/bg0.png); background-size: cover;">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <h2><span style="font-size: 1.3em; font-family: 'Lobster', cursive; color: white; text-shadow: 0 0 10px black;">FayVideo</span></h2>
+              <h5><strong>для Интернет-Предпринимателей<br /> и Владельцев Онлайн Магазинов</strong></h5>
+            </div>
+            <div class="col-md-6">
+              <h3 class="pull-right" style="text-align: right; font-family: 'Lobster'; color: #7af; text-shadow: 0 0 1px black;">
+                  +38(066)8252498,
+                  +78(999)9999999
+                  fayviideo@gmail.com
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="main" style="background: url(img/bg1.png); background-size: cover;">
+        <div class="container">
+          <h2 class="page-header center-block" style="font-family: 'Andika', sans-serif; font-size: 3em;">
+            <b>Закажите Продающее Видео на 1-2 Минуты, которое Увеличит Вашу Конверсию на <strong style="color: orange;">64-85%</strong>
+            и Принесет Вам в <strong style="color: orange;">2-4 Раза</strong> Больше Клиентов в 2015-2016 Годах<b/> <br />
+            <small style="color:white;font-family: arial; font-size: .6em;">Производство Продающих Видео по всей Территории СНГ</small>
+            <img class="arrow" style="margin-left: 0.8em;position: absolute; "src="img/arrow.png" alt="arrow"/>
+          </h2>
+          <div class="row">
+            <div class="col-md-7" style="padding-top: 8%; position: relative;">
+              <div class="absolute" style="left:100px; top: 40px">
+                <span style="font-size: 1.4em; font-family: 'Marck Script', cursive;">Как это работает?</span>
+              </div>
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ePbKGoIGAXY"></iframe>
+              </div>
+            </div>
+            <div class="col-md-5">
+              <form role="form" action="" class="center-block" method="post" style="width: 90%; padding: 10% 13%; background-color: #dfd; color: black; box-shadow: inset 0 0 20px black;">
+                <h3 style="text-align: center; color: white; text-shadow: 0 0 8px black;">Внесите Ваше Имя, E-mail, моб. тел ниже</h3><br />
+                <div class="form-group">
+                  <input type="email" class="form-control" id="email" placeholder="Введите Ваш e-mail">
+                </div>
+                <div class="form-group">
+                  <input type="name" class="form-control" id="name" placeholder="Введите Ваше имя">
+                </div>
+                <div class="form-group">
+                  <input type="phone" class="form-control" id="phone" placeholder="Введите Ваш телефон">
+                </div><br />
+                <button type="submit" class="btn-lg center-block" style="font-size:2em;">ЗАКАЖИТЕ</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="why-we">
+        <div class="container" style="position: relative;">
+          <h2 class="page-header center-block padding">Почему Стоит Заказывать у Нас?</h2>
+          <div class="row">
+            <div class="col-md-6 adv">
+              <img src='img/why-we/video.png' alt="video" /><h4>Мы создаем Видео <strong>Любой</strong> Сложности</h4>
+            </div>
+            <div class="col-md-6 adv">
+              <img src='img/why-we/conversia.png' alt="conversia"/><h4>Наше Видео Увеличивает Конверсию от <strong>64 до 85%</strong></h4>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 adv">
+              <img src='img/why-we/workwith.png' alt="workwith"/><h4>Нашими Услугами пользуются <strong>Крупнейшие</strong> Интернет Магазины СНГ и <strong>Телевидение</strong></h4>
+            </div>
+            <div class="col-md-6 adv">
+              <img src='img/why-we/tech.png' alt="tech"/><h4>Мы используем Технологии <strong>Западного</strong> Рынка которые Обкатаны до Автоматизма</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="stock">
+        <div class="container">
+          <h1 style="color: #f14; text-shadow: none; font-size: 3em;"><strong>АКЦИЯ!</strong></h1>
+          <div class="row">
+            <div class="col-md-6">
+              <h3>Сделайте Заказ до Того как Таймер Покажет <strong>00:00:00</strong> и Получите Бесплатно PDF-Инструкцию <strong>
+              <br /><br />
+              <img src="img/pdf.png" alt="pdf" style="width: 30%; height: 30%;" />
+              <img src="img/arrow3.png" style="width: 40%; height: 80px; float: right;"/>
+              <br /><br />
+              <i>"Бесплатные Источники Привлечения Клиентов, которые Вы Использовали Только на 10% Либо Не Использовали Вообще! [от А до Я]"</i></strong></h3>
+            </div>
+            <div class="col-md-6">
+              TIMER<br /><br /><br />
+              <button style="width: 90%;" type="button" name="button" class="btn-lg center-block button-large">ХОЧУ ВИДЕО</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="aboutus">
+        <div class="container">
+          <h2 class="page-header center-block padding">О Нас Говорят</h2>
+          <div class="row" style="margin: 7% 0;">
+            <div class="col-md-4">
+              <img src="img/bg0.png" style="width: 90%;"/>
+            </div>
+            <div class="col-md-8">
+              <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
+            </div>
+          </div>
+          <div class="row" style="margin: 7% 0;">
+            <div class="col-md-8">
+              <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
+            </div>
+            <div class="col-md-4">
+              <img src="img/bg0.png" style="width: 90%;"/>
+            </div>
+          </div>
+          <div class="row" style="margin: 7% 0;">
+            <div class="col-md-4">
+              <img src="img/bg0.png" style="width: 90%;"/>
+            </div>
+            <div class="col-md-8">
+              <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="clients">
+        <div class="container">
+          <h2 class="page-header center-block padding">Некоторые из Наших Клиентов...</h2>
+            <div class="row">
+              <div class="col-md-2">
+                <img src="img/urgatravel.png" alt="urgatravel" style="width:120%;"/>
+              </div>
+              <div class="col-md-8">
+                <div class="row">
+                  <div class="col-md-6 image">
+                    <img src="img/acmlv.png" alt="acmlv" style="width:80%;"/>
+                  </div>
+                  <div class="col-md-6 image">
+                    <img src="img/medialook.png" alt="medialook" style="width:90%"/>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6 image">
+                    <img src="img/cityguide.png" alt="cityguide" style="width:80%;height: 60px;"/>
+                  </div>
+                  <div class="col-md-6 image">
+                    <img src="img/sngb.png" alt="sngb" />
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-2">
+                <img src="img/shownabiz.png" alt="shownabiz" style="height:130px;"/>
+              </div>
+            </div>
+        </div>
+      </div>
+
+      <div class="brands">
+        <div class="container">
+          <h2 class="page-header center-block padding">Просто Гляньте... </h2>
+          <h3>Видеопрезентации Используют Такие <strong>Гиганты</strong> Как...</h3>
+          <ul class="list-inline">
+            <li>
+              <h4>APPLE</h4>
+              <img width="400" src="img/apple.png" alt="" />
+            </li>
+            <li>
+              <h4>DOLCE&GABBANA</h4>
+              <img width="400" src="img/dolcegabbana.png" alt="" />
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="workprocess">
+        <div class="container">
+          <h2 class="page-header center-block padding">Как Мы Работаем</h2>
+          <div class="row">
+            <div class="col-md-4">
+              <h3><span style="color: orange; font-size: 2em;">1.</span> Вы оставляете свои данные</h3>
+            </div>
+            <div class="col-md-4">
+              <h3><span style="color: orange; font-size: 2em;">2.</span> Наш менеджер связывается с Вами</h3>
+            </div>
+            <div class="col-md-4">
+              <h3><span style="color: orange; font-size: 2em;">3.</span> Мы с Вами уточняем Детали и согласовываем Бюджет</h3>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <h3><span style="color: orange; font-size: 2em;">4.</span> Мы делаем Ролик и Вы его Принимаете</h3>
+            </div>
+            <div class="col-md-4">
+              <h3><span style="color: orange; font-size: 2em;">5.</span> Мы пишем Сценарий и Вы его Утверждаете</h3>
+            </div>
+            <div class="col-md-4">
+              <h3><span style="color: orange; font-size: 2em;">6.</span> Вы удивляетесь Количеству новых Клиентов</h3>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="stock2">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <h1 style="color: #f14; text-shadow: none; font-size: 3em;"><strong>АКЦИЯ!</strong></h1>
+              <h3>Заполняйте форму Сейчас и Получите Бесплатно PDF-Инструкцию <strong><br /><br />
+              <i>"Бесплатные Источники Привлечения Клиентов, которые Вы Использовали Только на 10% Либо Не Использовали Вообще! [от А до Я]"</i></strong></h3>
+              <img src="img/arrow3.png" style="width: 80%; height: 180px; float: right;" alt="arrow" />
+            </div>
+            <div class="col-md-6">
+              <form role="form" action="" class="center-block" method="post" style="width: 90%; padding: 10% 13%; background-color: #dfd; color: black; box-shadow: inset 0 0 20px black;">
+                <h3 style="text-align: center; color: white; text-shadow: 0 0 8px black;">Внесите Ваше Имя, E-mail, моб. тел ниже</h3><br />
+                <div class="form-group">
+                  <input type="email" class="form-control" id="email" placeholder="Введите Ваш e-mail">
+                </div>
+                <div class="form-group">
+                  <input type="name" class="form-control" id="name" placeholder="Введите Ваше имя">
+                </div>
+                <div class="form-group">
+                  <input type="phone" class="form-control" id="phone" placeholder="Введите Ваш телефон">
+                </div><br />
+                <button type="submit" class="btn-lg center-block" style="font-size:2em;">ЗАКАЖИТЕ</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="whatwedo">
+        <div class="container">
+          <h2 class="page-header center-block padding">Какие Видео Вы Можете Заказать</h2>
+          <div class="row">
+            <div class="col-md-6">
+              <ul>
+                <li>Презентацию Вашего Товара или Услуги</li>
+                <li>Видео из Фотографий</li>
+                <li>Анимационное видео</li>
+                <li>Инфографику</li>
+              </ul>
+            </div>
+            <div class="col-md-6">
+              <ul>
+                <li>Презентацию Вашего Сайта</li>
+                <li>Видеообзор</li>
+                <li>Рекламу для Телевидения</li>
+                <li>2D и 3D Графику</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="examples">
+        <div class="container">
+          <h2 class="page-header center-block padding">Некоторые Из Наших Работ...</h2>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/DhhHdYvQaAo"></iframe>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/eYi9WtMTn8E"></iframe>
+              </div>
+            </div>
+          </div>
+          <br>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/XdWdyCRxEXs"></iframe>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/PRUrUZm7Vy0"></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="callforaction">
+        <div class="container">
+          <div class="row">
+            <h2 class="page-header center-block padding">Хотите Сделать Заказ?</h2>
+            <div class="col-md-6">
+              <h3>Заполняйте Форму Справа Прямо  и Получите Бесплатную Консультацию</h3><br />
+              <h3>А так же Ценный Бонус по Привлечению Клиентов!</h3>
+              <img src="img/arrow3.png" style="width: 80%; height: 120px; float: right;"/>
+            </div>
+            <div class="col-md-6">
+              <form role="form" action="" class="center-block" method="post" style="width: 90%; padding: 10% 13%; background-color: #dfd; color: black; box-shadow: inset 0 0 20px black;">
+                <h3 style="text-align: center; color: white; text-shadow: 0 0 8px black;">Внесите Ваше Имя, E-mail, моб. тел ниже</h3><br />
+                <div class="form-group">
+                  <input type="email" class="form-control" id="email" placeholder="Введите Ваш e-mail">
+                </div>
+                <div class="form-group">
+                  <input type="name" class="form-control" id="name" placeholder="Введите Ваше имя">
+                </div>
+                <div class="form-group">
+                  <input type="phone" class="form-control" id="phone" placeholder="Введите Ваш телефон">
+                </div><br />
+                <button type="submit" class="btn-lg center-block" style="font-size:2em;">ЗАКАЖИТЕ</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer" style="background: url(img/bg0.png); margin-top: 5%; padding: 2% 0;">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <h2><span style="font-size: 1.3em; font-family: 'Lobster', cursive; color: white; text-shadow: 0 0 10px black;">FayVideo</span></h2>
+              <h5><strong>для Интернет-Предпринимателей<br /> и Владельцев Онлайн Магазинов</strong></h5>
+            </div>
+            <div class="col-md-6">
+              <h3 class="pull-right" style="text-align: right; font-family: 'Lobster'; color: #7af; text-shadow: 0 0 1px black;">
+                  +38(066)8252498,
+                  +78(999)9999999
+                  fayviideo@gmail.com
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
