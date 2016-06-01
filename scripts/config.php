2c052ac614d326956657d804cd8d6ef265acf970
Yurii <imafost758@gmail.com>
2016-06-01 11:59:47 +0300

    new email added

--- scripts/config.php
+++ scripts/config.php
@@ -1,5 +1,5 @@
 <?php
-    define('MAIL_TO', 'imafos_t@ukr.net');
+    define('MAIL_TO', 'imafos_t@ukr.net, gatipe89@gmail.com');
     define('ACT_SUBJECT', 'Заказ Оформлен!');
     define('SUBJECT', 'Новый Заказ!');
     define('DENIED', 'Ошибка доступа');
